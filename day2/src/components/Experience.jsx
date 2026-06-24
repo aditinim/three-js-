2385a68f13d53a5d8b9bff9dce1c1a9d90323fa6
Aditi Nim <nimaditi7@gmail.com>
2026-06-25 01:40:25 +0530

    group

--- day2/src/components/Experience.jsx
+++ day2/src/components/Experience.jsx
@@ -2,22 +2,19 @@ import React from 'react'
 
 
 const Experience = () => {
-  return <>
-    <mesh position={[-1,4,0]}>
-        <boxGeometry args={[1,3,1]}/>
-        <meshBasicMaterial color= {"red"}/>
-    </mesh>
+    return <>
+        <group position={[1,2,0]}>
+            <mesh position={[-1, 4, 0]}>
+                <boxGeometry args={[1, 3, 1]} />
+                <meshBasicMaterial color={"red"} />
+            </mesh>
 
-    <mesh position={[-1,4,4]}>
-        <boxGeometry args={[1,3,1]}/>
-        <meshBasicMaterial color= {"red"}/>
-    </mesh>
-
-    <mesh>
-        <boxGeometry args={[1,3,1]}/>
-        <meshBasicMaterial color= {"green"}/>
-    </mesh>
-  </>
+            <mesh>
+                <boxGeometry args={[1, 3, 1]} /> 
+                <meshBasicMaterial color={"green"} />
+            </mesh>
+        </group>
+    </>
 }
 
 export default Experience
